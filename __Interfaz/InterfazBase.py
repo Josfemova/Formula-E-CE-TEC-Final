a6ccfff7661626038403e04f3a74134ba97835a9
AleQuesada2012 <49792796+AleQuesada2012@users.noreply.github.com>
2019-05-20 14:14:44 -0600

    Cambio en dimensiones de encabezado y ventanas
    Se redujo el tamaño de las ventanas, y se utilizó un cambio de línea en el comentario para hacer un corte manual en el espacio que estas abarcan en el ScrolledText

--- __Interfaz/InterfazBase.py
+++ __Interfaz/InterfazBase.py
@@ -62,11 +62,13 @@ Versión del programa: 1.0.0
 
 Versión de Python: 3.7.2
 ____________________________________
-Hints: Verifique que el auto encendió correctamente.
-Verifique que tanto el equipo de telemetría como el auto estén
+Hints: Verifique que el auto encendió
+correctamente.
+Verifique que tanto el equipo de telemetría
+como el auto estén
 conectados a la misma red inalámbrica.
-Utilice las teclas WASD para mover el auto, seleccione un piloto
-e inicie su Test Drive.
+Utilice las teclas WASD para mover el auto.
+seleccione un piloto e inicie su Test Drive.
 """
 #    __________________________
 #___/Importación de Bibliotecas
@@ -90,12 +92,12 @@ def cargar_imagen(Nombre):
 #___/Creación de Ventana Principal
 Main = Tk() #Se asigna una función de Tkinter al nombre Main
 Main.title("Home")
-Main.geometry('1370x768')
+Main.geometry('800x600')
 Main.resizable(width=NO,height=NO)
 
 #    _______________________________
 #___/Canvas para trabajar la ventana
-MainCanv = Canvas(Main,width=1370,height=768,bg="white")
+MainCanv = Canvas(Main,width=1370,height=768,bg="grey")
 MainCanv.place(x=0,y=0)
 
 #Insertar código que cargue la imagen del fondo en el canvas
@@ -118,29 +120,32 @@ def btn_pilots():
 def about_window():
     About = Toplevel()
     About.title("About")
-    About.minsize(1370,768)
+    About.minsize(800,600)
     About.resizable(width= NO, height= NO)
-    AboutCanv = Canvas(About, width= 1370, height= 768,bg="white")
+    AboutCanv = Canvas(About, width= 800, height= 600,bg="grey")
     AboutCanv.place(x=0,y=0)
-    Info = scrolledtext.ScrolledText(About,width= 40, height= 10) #no son pixeles hee hee
-    Info.insert(INSERT,AboutText)
-    Info.place(x = 600, y = 300)
+    Info = scrolledtext.ScrolledText(About,width= 40, height= 10,bg= "black", fg= "cyan") #no es una medida en pixeles
+    Info.insert(INSERT,AboutText) #Se añade el texto del comentario multilínea perteneciente a la variable, dentro del scrolledtext.
+    Info.place(x = 400, y = 10)
+    FotoAle = cargar_imagen("AlejandroPrueba.png")
+    AboutCanv.create_image(1,1,image=FotoAle,anchor = NW)
     #Se define una función para el botón de atrás a la principal
     def btn_back1():
         About.destroy()
         Main.deiconify() #Recargar la ventana principal
         
-    BtnBack1 = Button(AboutCanv, text= "Main", command = btn_back1,fg = "white", bg= "black")
-    BtnBack1.place(x = 100, y = 200)
+    BtnBack1 = Button(AboutCanv, text= "Main", command = btn_back1,fg = "cyan", bg= "black")
+    BtnBack1.place(x = 800, y = 300)
+    About.mainloop()
 
 #-----Se termina la venta about y se define la ventana de pruebas
 def test_drive_window():
     TestDrive= Toplevel()
     TestDrive.title("Test Drive")
-    TestDrive.minsize(width= 1370, height = 768)
+    TestDrive.minsize(width= 800, height = 600)
     TestDrive.resizable(width= NO, height= NO)
     #Se genera el canvas de la ventana de pruebas
-    TestCanv= Canvas(TestDrive, width= 1370, height= 768,bg="white")
+    TestCanv= Canvas(TestDrive, width= 800, height= 600,bg="grey")
     TestCanv.place(x=0, y=0)
     #Se debe programar la adición de las operaciones de la función aparte de generar la ventana
 
@@ -150,17 +155,17 @@ def test_drive_window():
         TestDrive.destroy()
         Main.deiconify()
         
-    BtnBack2= Button(TestCanv, text = "Main", command = btn_back2, fg= "White", bg= "black")
-    BtnBack2.place(x= 200, y =100)
+    BtnBack2= Button(TestCanv, text = "Main", command = btn_back2, fg= "cyan", bg= "black")
+    BtnBack2.place(x= 400, y =300)
 
 #-----Se termina la ventana de pruebas y se define la de los pilotos
 def pilots_window():
     Pilots = Toplevel()
     Pilots.title("Pilots")
-    Pilots.minsize(width= 1370, height= 768)
+    Pilots.minsize(width= 800, height= 600)
     Pilots.resizable(width= NO, height= NO)
     #Se genera el canvas
-    PilotsCanv= Canvas(Pilots, width = 1370, height = 768,bg="white")
+    PilotsCanv= Canvas(Pilots, width = 800, height = 600,bg="grey")
     PilotsCanv.place(x=0,y=0)
     #Se debe programar el resto del funcionamiento del módulo antes de declararlo como listo
 
@@ -169,17 +174,17 @@ def pilots_window():
         Pilots.destroy()
         Main.deiconify()
         
-    BtnBack3= Button(PilotsCanv, text ="Main", command = btn_back3, fg= "white", bg = "black")
+    BtnBack3= Button(PilotsCanv, text ="Main", command = btn_back3, fg= "cyan", bg = "black")
     BtnBack3.place(x= 200, y =100)
 
 #------Comandos de los Botones en Main------
-BtnAbout= Button(MainCanv, text= "About", command = btn_about,fg= "white",bg ="black") #Para ir a About
+BtnAbout= Button(MainCanv, text= "About", command = btn_about,fg= "cyan",bg ="black") #Para ir a About
 BtnAbout.place(x = 100,y = 100)
 
-BtnTest= Button(MainCanv, text= "Test Drive", command = btn_test,fg= "white",bg ="black")  #Para ir a TestDrive
+BtnTest= Button(MainCanv, text= "Test Drive", command = btn_test,fg= "cyan",bg ="black")  #Para ir a TestDrive
 BtnTest.place(x = 200, y = 100)
 
-BtnPilots = Button(MainCanv, text= "Pilots", command = btn_pilots,fg= "white",bg ="black") #Para ir a Pilots
+BtnPilots = Button(MainCanv, text= "Pilots", command = btn_pilots,fg= "cyan",bg ="black") #Para ir a Pilots
 BtnPilots.place(x = 300, y = 100)
     
 Main.mainloop()
